d53bceb4ed7ebabb2406a5cc14f7e96026e5e330
Liquidibrium <vanoganjelashvili@gmail.com>
2021-06-07 18:57:14 +0400

    add swagger UI for api

---
+++ bank-backend/src/main/java/ge/bog/bank/backend/config/SpringFoxConfig.java
@@ -0,0 +1,22 @@
+package ge.bog.bank.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.ant("/api/**"))
+                .apis(RequestHandlerSelectors.basePackage("ge.bog"))
+                .build();
+    }
+}
